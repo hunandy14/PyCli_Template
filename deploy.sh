bedd30f789cc2553d5fc37a53871aa619e032102
Charlotte.Hong <hunandy14@gmail.com>
2024-06-16 22:53:08 +0900

    改進 deploy shell 語法

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,43 @@
 #!/bin/bash
 
 # 確認參數 Path 是否已提供，若無則設置預設值
-if [ -z "$1" ]; then
-    Path="./.venv"
-else
-    Path="$1"
-fi
+Path=${1:-"./.venv"}
+
+# 定義函數
+info() { echo -e "\e[90m$1\e[0m"; }
+warning() { echo -e "\e[33mWARNING: $1\e[0m"; }
+success() { echo -e "\e[32m$1\e[0m"; }
+error() { echo -e "\e[31mERROR: $1\e[0m"; exit 1; }
 
 # 檢查 requirements.txt 是否存在
-if [ ! -f "requirements.txt" ]; then
-    echo "requirements.txt not found."
-    exit 1
-fi
+[[ -f "requirements.txt" ]] || error "requirements.txt not found."
+
+# 檢查是否有 python3
+command -v python3 &> /dev/null || error "Python 3 is not installed."
 
 # 檢查虛擬環境是否已存在
-if [ -f "$Path/bin/activate" ]; then
-    echo -e "\e[33mWARNING: Found existing virtual environment at '$Path'. Using it to install dependencies.\e[0m"
+if [[ -f "$Path/bin/activate" ]]; then
+    warning "Found existing virtual environment at '$Path'. Using it to install dependencies."
 else
-    echo -e "\e[90mCreating a new virtual environment at '$Path'.\e[0m"
-    # 建立虛擬環境
-    python3 -m venv $Path
-    if [ $? -ne 0 ]; then
-        echo -e "\e[31mERROR: Failed to create the virtual environment at '$Path'.\e[0m"
-        exit 1
-    fi
+    info "Creating a new virtual environment at '$Path'."
+    python3 -m venv "$Path" || error "Failed to create the virtual environment at '$Path'."
 fi
 
 # 啟動虛擬環境
-echo -e "\e[90mActivating the virtual environment at '$Path'...\e[0m"
-source "$Path/bin/activate"
-if [ $? -ne 0 ]; then
-    echo -e "\e[31mERROR: Failed to activate the virtual environment at '$Path'.\e[0m"
-    exit 1
-fi
+info "Activating the virtual environment at '$Path'..."
+source "$Path/bin/activate" || error "Failed to activate the virtual environment at '$Path'."
 
 # 安裝 requirements.txt 中的依賴
-echo -e "\e[90mInstalling dependencies from requirements.txt...\e[0m"
-pip install -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo -e "\e[31mERROR: Failed to install dependencies from requirements.txt.\e[0m"
+info "Installing dependencies from requirements.txt..."
+"$Path/bin/pip" install -r requirements.txt || {
+    error "Failed to install dependencies from requirements.txt."
     deactivate
-    exit 1
-fi
+}
 
-echo -e "\e[32mVirtual environment '$Path' successfully created and dependencies installed.\e[0m"
+success "Virtual environment '$Path' successfully created and dependencies installed."
 
 # 離開虛擬環境並恢復原本狀態
 deactivate
 
-echo -e "\e[90mTo activate the virtual environment, run the following command:\e[0m"
-echo -e "\e[90msource '$Path/bin/activate'\e[0m"
+info "To activate the virtual environment, run the following command:"
+info "source '$Path/bin/activate'"
